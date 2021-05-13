67adee8cd89b1aebeb518a1826c6a9412f0b5b7c
Vann09 <izquierdoesteban1@gmail.com>
2021-05-13 10:36:08 +0200

    Add project files.

---
+++ Formacion.CSharp.ConsolaApp1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Formacion.CSharp.ConsolaApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
